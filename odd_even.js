7eccd53fcf92c0cd18e55b2a6497b69d0d03dc0b
anuraag0x <anurag.kushwah@kapiva.in>
2025-03-10 20:35:15 +0530

    added a new file add to numbers

--- odd_even.js
+++ odd_even.js
@@ -1,7 +1,10 @@
+import {addNumbers} from "./addToNumbers.js";
 //even and odd number
-let num=10;
-if(num%2===0){
-    console.log("Even Number")
-}else{
-    console.log("Odd Number")
+let num = 10;
+if (num % 2 === 0) {
+  console.log("Even Number");
+} else {
+  console.log("Odd Number");
 }
+let ans = addNumbers(115, 20);
+console.log(ans);
